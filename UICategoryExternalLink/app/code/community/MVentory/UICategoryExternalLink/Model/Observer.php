7e6cfe0cefd433e3dc9172afadd2f75e18f4fd59
Anatoly A. Kazantsev <anatoly.kazantsev@gmail.com>
2015-10-19 23:19:17 +1300

    Add licence

--- UICategoryExternalLink/app/code/community/MVentory/UICategoryExternalLink/Model/Observer.php
+++ UICategoryExternalLink/app/code/community/MVentory/UICategoryExternalLink/Model/Observer.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License BY-NC-ND.
+ * NonCommercial  You may not use the material for commercial purposes.
+ * NoDerivatives  If you remix, transform, or build upon the material,
+ * you may not distribute the modified material.
+ * See the full license at http://creativecommons.org/licenses/by-nc-nd/4.0/
+ *
+ * See http://mventory.com/legal/licensing/ for other licensing options.
+ *
+ * @package MVentory/UICategoryExternalLink
+ * @copyright Copyright (c) 2015 mVentory Ltd. (http://mventory.com)
+ * @license http://creativecommons.org/licenses/by-nc-nd/4.0/
+*/
+
 class MVentory_UICategoryExternalLink_Model_Observer
   extends Mage_Catalog_Model_Observer
 {
